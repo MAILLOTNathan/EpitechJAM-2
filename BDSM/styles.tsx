566f6461e20cae03193a77ef123860da5cdd38fa
MAILLOTNathan <nathan.maillot@epitech.eu>
2024-03-22 23:13:27 +0400

    feat: add NavBar

--- BDSM/styles.tsx
+++ BDSM/styles.tsx
@@ -7,6 +7,24 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    mybutton: {
+        backgroundColor: '#F79A24',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 50,
+        padding: 20,
+        margin: 5,
+    },
+    navbar: {
+        display: 'flex',
+        flexDirection: 'row',
+        backgroundColor: '#666666',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginHorizontal: 'auto',
+        borderRadius: 5,
+        padding: 10,
+    },
 });
 
 export default styles;
