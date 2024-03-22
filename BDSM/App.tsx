03eefb1ca3251737cb819e9254e96b869bd8b0f8
LAURET Benjamin <benjamin.lauret@epitech.eu>
2024-03-22 23:14:28 +0400

    add: V1 of profil_section

--- BDSM/App.tsx
+++ BDSM/App.tsx
@@ -3,12 +3,12 @@ import { Text, View } from 'react-native';
 
 import styles from './styles';
 import NavBar from './Components/NavBar';
+import Profil from './Components/Profil';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <NavBar>
-      </NavBar>
+      <Profil />
     </View>
   );
 }
--- BDSM/styles.tsx
+++ BDSM/styles.tsx
@@ -7,6 +7,53 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    cont: {
+        backgroundColor: '#fff',
+      },
+      header: {
+        marginTop:50,
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 20,
+      },
+      avatar: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+      },
+      info: {
+        marginLeft: 20,
+      },
+      name: {
+        fontSize: 24,
+        fontWeight: 'bold',
+      },
+      username: {
+        color: '#999',
+        fontSize: 18,
+      },
+      stats: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 20,
+      },
+      stat: {
+        flex: 1,
+        margin: 10,
+        alignItems: 'center',
+      },
+      statLabel: {
+        color: '#999',
+        fontSize: 14,
+      },
+      statValue: {
+        fontSize: 18,
+      },
+      bio: {
+        padding: 20,
+        fontSize: 16,
+        color: '#333',
+      },
 });
 
 export default styles;
