02a58671e174d89a2872d34bf13fc03c7183d065
LAURET Benjamin <benjamin.lauret@epitech.eu>
2024-03-23 10:47:45 +0400

    Add: V1 camera module

--- BDSM/Components/Home.tsx
+++ BDSM/Components/Home.tsx
@@ -1,15 +1,40 @@
 import { View, Text } from "react-native"
+import { Camera } from 'expo-camera';
 
 import Map from "./Map";
 
 import styles from "../styles";
+import { useEffect, useState } from "react";
+import React from "react";
 
 export default function Home() {
+
+    let x = false;
+    let d = Camera.Constants.Type;
+    const [hasPermission, setHasPermission] = useState(x);
+    const [type, setType] = useState(d);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            let z = (status === 'granted');
+            setHasPermission(z);
+        })();
+    }, []);
+
+    let y = hasPermission === null
+    if (y) {
+        return <View />;
+    }
+    let p = hasPermission === false
+    if (p) {
+        return <Text>No access to camera</Text>;
+    }
+
     return (
         <View style={styles.container}>
-            <Text>
-                KBO
-            </Text>
+            <Camera style={{ flex: 1 }} type={type}>
+            </Camera>
         </View>
     )
 }
