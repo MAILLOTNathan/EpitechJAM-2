8d5557102bdd091455c09f6b87f7246ffeadda6c
LAURET Benjamin <benjamin.lauret@epitech.eu>
2024-03-23 10:16:10 +0400

    Fix: Double key package.json

--- BDSM/App.tsx
+++ BDSM/App.tsx
@@ -9,6 +9,7 @@ import Home from './Components/Home';
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from 'react';
 
 const Stack = createNativeStackNavigator();
 
