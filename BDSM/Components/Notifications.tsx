a2bc0498ee0c86ab3f4ffba19fac65fa3604ccf9
MAILLOTNathan <nathan.maillot@epitech.eu>
2024-03-24 15:35:27 +0400

    feat: link between db and notifications

--- BDSM/Components/Notifications.tsx
+++ BDSM/Components/Notifications.tsx
@@ -1,4 +1,7 @@
-import { View, Text, ScrollView, Touchable, TouchableOpacity } from "react-native";
+import { useState, useEffect } from "react";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+
+import axios from "axios";
 
 import styles from "../styles";
 
@@ -77,23 +80,53 @@ const data = [
     },
 ];
 
-function createNotifications (jsonData) {
+interface  INotif
+    {
+        user_name: string,
+        content: string,
+        url: string
+    };
+
+function createNotifications (jsonData: INotif[]) {
     return jsonData.map((item) => {
         return (
             <TouchableOpacity style={styles.mylongbutton}>
-                <Text>{item.name}</Text>
-                <Text>{item.content}</Text>
+                <Text>{item.user_name}</Text>
+                <Text>posted.</Text>
             </TouchableOpacity>
         );
     });
 }
 
 export default function  Notifications () {
+    const [feed, setFeed] = useState<INotif[]>([]);
+
+    const getDatabaseEntry = async (): Promise<INotif[]> => {
+        try {
+        const response = await axios.get<INotif[]>('http://10.106.1.24:3000/post');
+            console.log('Database entry created:', response.data)
+            return response.data
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            throw new Error('Failed to fetch data');
+        }
+    };
+
+    useEffect( () => {
+        const fetchdata = async () => { try {
+            const ListOfData = await getDatabaseEntry();
+            setFeed(ListOfData)
+        }
+        catch(error) {
+            console.log(error)
+        }
+        }; fetchdata();
+    }, [] )
     return (
         <View style={styles.container}>
             <View style={styles.notificationpanel}>
                 <ScrollView>
-                    {createNotifications(data)}
+                    {createNotifications(feed)}
                 </ScrollView>
             </View>
         </View>
